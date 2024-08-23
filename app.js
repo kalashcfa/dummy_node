0a49b79a312021e8a1aa11e4713d66178b49a719
kalashcfa <kalashchourasiya.cfa@gmail.com>
2024-08-23 16:17:13 +0530

    async await

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-const l_dash = require('lodash')
+const file = require ('fs');
+const utils = require('util')
+const PATH = require('path');
 
-const arr = [1, [2, [3, [4, [5]]]]]
-console.log(arr[0].[0]);
 
-const newArr = l_dash.flattenDeep(arr)
-console.log(newArr)
+const readFilePromise = utils.promisify(file.readFile);
+const writeFilePromise = utils.promisify(file.writeFile);
+
+const filePath1 = PATH.join('./content','first.txt')
+const filePath2= PATH.join('./content','second.txt')
+
+// const getFile = (path) =>
+// {
+//     return new Promise ((resolve , reject) =>
+//     {
+//         file.readFile(path , 'utf-8' , (err , res)=>
+//         {
+//             if(err)
+//             {
+//                 reject(err)
+//             }
+//             else
+//             {
+//                 resolve(res)
+//             }
+//         })
+//     })
+// }
+
+
+const start =  async ()=>
+    {
+        try
+        {
+            const first = await readFilePromise(filePath1, 'utf-8');
+            const seconnd = await readFilePromise(filePath2, 'utf-8') 
+            console.log('file data :',first, ' \n', seconnd)
+        }
+        catch(err) 
+        {
+            console.log(err)
+        }
+    } 
+
+start()
