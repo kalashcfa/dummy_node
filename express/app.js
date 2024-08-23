b7103158095a961c2b0918870436e1d5676cdabf
kalashcfa <kalashchourasiya.cfa@gmail.com>
2024-08-23 20:16:21 +0530

    install express

--- express/app.js
+++ express/app.js
@@ -1,16 +1,32 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
     console.log("User hit the server !");
-    res.write(`
-    <h1>
-        Home page 
-    </h1>`);
-    res.end();
+    console.log(req.method)
+    const url = req.url;
+    if(url === '/')
+    {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write(`
+            <h1>
+                Home page 
+            </h1>`);
+        res.end();
+    }
+    else
+    {
+        res.writeHead(404 , {"content-type": "text/html"})
+        res.write(`
+            <h1>
+                Page not found
+            </h1>            
+            `)
+        res.end();
+    }
 
 });
 
 server.listen(5001, () => {
     console.log("server started");
 });
+  
